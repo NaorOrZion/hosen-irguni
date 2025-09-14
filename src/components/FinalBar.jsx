b6945cde3d02ad9a1a34b4316954503d24115155
NaorOrZion <naor5506@gmail.com>
2025-09-14 16:28:36 +0300

    reradrf

--- src/components/FinalBar.jsx
+++ src/components/FinalBar.jsx
@@ -44,17 +44,10 @@ function FinalBar({ score }) {
       const svg = `
     <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
       <rect width="100%" height="100%" fill="#384d22" rx="40"/>
-      <image 
-        href="${svgDataUri}" 
-        x="${width / 2 - 220 / 2}" 
-        y="80" 
-        width="${220}" 
-        preserveAspectRatio="xMidYMid meet"
-      />
 
-      <text x="50%" y="60%" text-anchor="middle" font-size="36" font-family="Rubik,Arial,sans-serif" fill="#fff" direction="rtl">${mockData.textBeforeGrade}</text>
-      <text x="50%" y="75%" text-anchor="middle" font-size="80" font-family="Rubik,Arial,sans-serif" fill="#fff" direction="rtl" font-weight="bold">${score}</text>
-      <text x="50%" y="90%" text-anchor="middle" font-size="36" font-family="Rubik,Arial,sans-serif" fill="#fff" direction="rtl">${getLightText()}</text>
+      <text x="50%" y="20%" text-anchor="middle" font-size="36" font-family="Rubik,Arial,sans-serif" fill="#fff" direction="rtl">${mockData.textBeforeGrade}</text>
+      <text x="50%" y="45%" text-anchor="middle" font-size="80" font-family="Rubik,Arial,sans-serif" fill="#fff" direction="rtl" font-weight="bold">${score}</text>
+      <text x="50%" y="65%" text-anchor="middle" font-size="36" font-family="Rubik,Arial,sans-serif" fill="#fff" direction="rtl">${getLightText()}</text>
     </svg>
   `;
 
